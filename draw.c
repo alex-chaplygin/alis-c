a614e680116e8f8a0473bee31e8cd7d206e71ab4
alex-chaplygin <alex_chaplygin@mail.ru>
2022-02-08 11:09:01 +0300

    Модуль отрисовки

---
+++ draw.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
+#include "vector.h"
+#include "image.h"
+#include "graphics.h"
+
+#define IMAGE_FILL 1		/**< заполнение постоянным цветом */
+#define IMAGE_8_A 0x14		/**< 8 бит с прозрачностью */
+
+int image_add;			/**< смещение на следующую строчку изображения */
+int video_add;			/**< семещение на следующую строчку для видео буфера */
+int num_cols;			/**< число столбцов */
+int num_rows;			/**< число строк */
+byte *blit_src;			/**< указатель на изображение */
+byte *blit_dst;			/**< указатель на видеобуфер */
+
+/** 
+ * Настройка отрисовки. Вычисляет число строк и столбцов, 
+ * смещения для видео буфера и для изображения, указатели для видео
+ * буфера и изображения.
+ * @param origin левый верхний угол спрайта
+ * @param im данные изображения (заголовок + данные)
+ * @param x_flip если 1 - зеркальное отражение
+ * @param blit прямоугольник экранных координат, куда будет выведено
+ * изображение
+ */
+void draw_setup(vec_t *origin, image_t *im, int x_flip, rectangle_t *blit)
+{
+  image_add = im->maxx - blit->max_x + blit->min_x;
+  int video = SCREEN_WIDTH - 1 - blit->max_x + blit->min_x;
+  if (x_flip && im->type != IMAGE_FILL)
+    video_add = 2 * SCREEN_WIDTH - video;
+  else
+    video_add = video;
+  num_cols = blit->max_x - blit->min_x + 1;
+  num_rows = blit->max_y - blit->min_y + 1;
+  int pos = blit->min_y - origin->y;
+  if (pos > 0)
+    pos = pos * (im->maxx + 1);
+  int posx = blit->min_x - origin->x;
+  if (posx > 0)
+    pos += (x_flip) ? -posx : posx;
+  if (!x_flip)
+    pos += im->maxx + 1 - num_cols;
+  blit_src = (byte *)(im + 1) + pos;
+  blit_dst = video_buffer + blit->min_y * SCREEN_WIDTH + blit->min_x;
+#ifdef DEBUG
+  printf("draw setup: rows = %d cols = %d vid_add = %d im_add = %d x_fl = %d src = %d dst = %d\n", num_rows, num_cols, video_add, image_add, x_flip, (int)(blit_src - (byte *)im), (int)(blit_dst - video_buffer));
+#endif
+}
+
+/** 
+ * Заполняет вычисленную область постоянным цветом
+ * 
+ * @param color номер цвета
+ */
+void fill_image(byte color)
+{
+  for (int y = 0; y < num_rows; y++) {
+    for (int x = 0; x < num_cols; x++)
+      *blit_dst++ = color;
+    blit_dst += video_add;
+  }
+}
+
+void draw_image(int alpha)
+{
+  for (int y = 0; y < num_rows; y++) {
+    for (int x = 0; x < num_cols; x++)
+      if (alpha) {
+	byte c = *blit_src++;
+	if (c)
+	  *blit_dst++ = c;
+	else
+	  ++blit_dst;
+      } else
+	*blit_dst++ = *blit_src++;
+    blit_src += image_add;
+    blit_dst += video_add;
+  }
+}
+
+void render_image(vec_t *origin, image_t *im, int x_flip, rectangle_t *blit)
+{
+  switch (im->type) {
+  case IMAGE_FILL:
+    draw_setup(origin, im, x_flip, blit);
+    fill_image(im->fill_color);
+    break;
+  case IMAGE_8_A:
+    draw_setup(origin, im, x_flip, blit);
+    draw_image(1);
+    break;
+  default:
+    printf("Unknown image type: %x\n", im->type);
+    exit(1);
+  }
+}
---
+++ draw.h
@@ -0,0 +1,1 @@
+void render_image(vec_t *origin, image_t *im, int x_flip, rectangle_t *blit);
