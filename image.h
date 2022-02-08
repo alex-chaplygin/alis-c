261425c7dcee381b7450e90d11fe3eb1de06d012
alex-chaplygin <alex_chaplygin@mail.ru>
2022-02-08 11:10:30 +0300

    В структуру изображения добавлено поле
    для типа изображений 1 поле - цвет заполнения
    добавлена структура координатного окна

--- image.h
+++ image.h
@@ -6,12 +6,23 @@ enum resource_e {
 /// заголовок изображения
 typedef struct {
   byte type; 	/**< тип ресурса/изображения */
-  byte num_subimages;		/**< для составного изображения
+  union {
+    byte num_subimages;		/**< для составного изображения
 число компонент */
+    byte fill_color;		/**< для типа 1 - цвет заливки */
+  };
   word maxx;			/**< ширина - 1*/
   word maxy;			/**< высота - 1 */
   word brightness;
 } image_t;
+
+/// координатное окно
+typedef struct {
+  int min_x;			/**< левый верхний угол */
+  int min_y;
+  int max_x;			/**< правый нижний угол */
+  int max_y;
+} rectangle_t;
 
 void show_sprite();
 void show_sprite_flipped();
