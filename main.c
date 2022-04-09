d59daa3be07da5aeb10e4ee5ecfd64d99095df39
alex-chaplygin <alex_chaplygin@mail.ru>
2022-04-09 11:21:01 +0300

    Главная функция

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "sound.h"
 #include "video.h"
 #include "mouse.h"
+#include "vm.h"
+#include "threads.h"
 
 /** 
  * Инициализация всех подсистем
@@ -9,7 +11,18 @@
 void global_init()
 {
   load_blancpc();
-  sound_init();
   video_init();
+  sound_init();
   mouse_init();
+}
+
+void main()
+{
+  global_init();
+  threads_init();
+  vm_init();
+  while (1) {
+    threads_run();
+    video_update();
+  }
 }
