f2d8a48d2a8ef2d1d4956428ed7d99c2e36063b7
alex-chaplygin <alex_chaplygin@mail.ru>
2022-08-05 17:28:45 +0300

    Исправлена ошибка в функции - знак

--- math.c
+++ math.c
@@ -210,7 +210,7 @@ void sign()
 #ifdef DEBUG
   printf("sign %d\n", current_value);
 #endif
-  if (current_value > 0)
+  if (current_value != 0)
     current_value = (current_value > 0) ? 1 : -1;
 }
 
