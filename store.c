50d0c8f0f88a51372e3bdbce13a7edb82655931b
alex-chaplygin <alex_chaplygin@mail.ru>
2022-08-09 12:47:09 +0300

    Приведение типа short для индекса из массива

--- store.c
+++ store.c
@@ -168,7 +168,7 @@ void set_byte_array_word()
   word w = fetch_word();
   int idx = current_value;
   byte *pos = array_pos(seg_read(run_object->data, w), 0, 1);
-  current_value = (word)stack_pop(&stack);
+  current_value = (short)stack_pop(&stack);
   *pos = (byte)current_value;
 #ifdef DEBUG
   printf("store_b arrw_%x[%d], %x; %d\n", w, idx, (byte)current_value, (byte)current_value);
@@ -194,7 +194,7 @@ void set_byte_array_byte()
   byte w = fetch_byte();
   int idx = current_value;
   byte *pos = array_pos(seg_read(run_object->data, w), 0, 1);
-  current_value = (word)stack_pop(&stack);
+  current_value = (short)stack_pop(&stack);
   *pos = (byte)current_value;
 #ifdef DEBUG
   printf("store_b arrb_%x[%d], %x; %d\n", w, idx, (byte)current_value, (byte)current_value);
@@ -207,7 +207,7 @@ void set_word_array_byte()
   byte w = fetch_byte();
   int idx = current_value;
   word *pos = (word *)array_pos(seg_read(run_object->data, w), 0, 2);
-  current_value = (word)stack_pop(&stack);
+  current_value = (short)stack_pop(&stack);
   *pos = current_value;
 #ifdef DEBUG
   printf("store_w arrb_%x[%d], %x; %d\n", w, idx, current_value, current_value);
