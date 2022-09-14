c0955e28edf0e91d3666cbf69d40e40a3cb9e15c
alex-chaplygin <alex_chaplygin@mail.ru>
2022-09-14 17:39:44 +0300

    Добавление звука в систему

--- sound.c
+++ sound.c
@@ -17,6 +17,8 @@
 
 #define NUM_CHANNELS 4		/**< число каналов звука */
 #define DEFAULT_PRIORITY -128
+#define CHANNEL_OFF (1 << 7)	/**< флаг - канал выключен */
+#define CHANNEL_2 (1 << 1)
 
 typedef struct {
   byte flags;			/**< флаги канала */
@@ -28,6 +30,7 @@ typedef struct {
   short frequency;		/**< частота ноты */
   short frequency_gain;		/**< смещение частоты ноты во времени */
   int object;			/**< номер объекта, который послал звук */
+  int ready;
 } sound_channel_t;
 
 sound_channel_t  sound_channels[NUM_CHANNELS];	/**< каналы звука */
@@ -45,7 +48,37 @@ void init_sound_channels()
   }
 }
 
-void  add_sound(int pri, int obj)
+/** 
+ * Найти канал с минимальным приоритетом
+ * 
+ * @return канал
+ */
+sound_channel_t * find_minpri_channel()
+{
+  sound_channel_t *ch;
+  ch = &sound_channels[0];
+  if (ch->priority > sound_channels[1].priority)
+    ch = &sound_channels[1];
+  if (ch->priority > sound_channels[2].priority)
+    ch = &sound_channels[2];
+  if (ch->priority > sound_channels[3].priority)
+    ch = &sound_channels[3];
+  return ch;
+}
+
+/** 
+ * Добавление звука в систему
+ * 
+ * @param pri приоритет
+ * @param obj номер объекта
+ * @param ready готовность
+ * @param freq частота
+ * @param fgain смещение частоты
+ * @param vol громкость
+ * @param gain смещение амплитуды
+ * @param len длительность
+ */
+void  add_sound(int pri, int obj, int ready, int freq, int fgain, int vol, int gain, int len)
 {
   sound_channel_t *ch;
 #ifdef DEBUG
@@ -69,8 +102,28 @@ void  add_sound(int pri, int obj)
       exit(1);
     }
   }
-  ch = sound_channels;
-  for (int i = 0; i < NUM_CHANNELS; i++, ch++) {
+  ch = find_minpri_channel();
+  if (pri < 0 || !ch->priority || ch->priority == DEFAULT_PRIORITY) {
+    if (ch->priority > pri)
+      return;
+    ch->flags = CHANNEL_OFF;
+    ch->priority = pri;
+    ch->ready = ready;
+    ch->frequency = freq;
+    ch->frequency_gain = fgain;
+    ch->volume = vol << 8;
+    ch->gain = gain;
+    ch->length = len;
+    ch->object = obj;
+    ch->flags = CHANNEL_2;
+    if (ready & 0x80) {
+      printf("ready & 0x80\n");
+      exit(1);
+    }
+#ifdef DEBUG
+    printf("adding sound channel num = %d\n", (int)(ch - sound_channels));
+    printf("flags=%x pri=%x ready=%x freq=%x fgain=%x, vol=%x, gain=%x len=%x obj=%x\n", ch->flags, ch->priority, ch->ready, ch->frequency, ch->frequency_gain, ch->volume, ch->gain, ch->length, ch->object);
+#endif
   }
 }
 
@@ -79,23 +132,21 @@ void  add_sound(int pri, int obj)
  */
 void play_sound_synth()
 {
-  extern int freq;
-  extern int slen;
   new_get();
   int pri = (char)current_value;
   new_get();
   int vol = (char)current_value;
   new_get();
-  freq = current_value;
+  int freq = current_value;
   new_get();
-  slen = current_value;
+  int slen = current_value;
 #ifdef DEBUG
   printf("play_sound_synth pri=%d vol=%d freq=%d len=%d\n", pri, vol, freq, slen);
 #endif
   if (!slen)
     return;
   //  int s = (s2 << 8) % slen;
-  add_sound(pri, object_num(run_object));
+  add_sound(pri, object_num(run_object), 1, freq, 0, vol, -1, slen);
   exit(1);
 }
 
